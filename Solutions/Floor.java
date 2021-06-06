3d515c8159e8b9f22440e305cf4ad386f6a5ae9d
Owner <journeyjava0@gmail.com>
2021-06-05 22:29:33 -0500

    both parts to Advent of Code Day 1

--- Solutions/Floor.java
+++ Solutions/Floor.java
@@ -9,8 +9,12 @@ import java.util.List;
 public class Floor {
 
     public static int find_floor(String directions) {
-        //initialize counter variable
-        int count = 0;
+        //initialize variables to keep track of floor and position
+        int whichFloor = 0; //keeps track of which floor we're on
+        int position = 1; //keeps track of position of ( or ) each time we enter the basement
+
+        //create a list of all the positions that we enter the basement
+        List<Integer> positionsList = new ArrayList<>();
 
         //convert the string to an array of chars
         List<Character> chars = new ArrayList<>();
@@ -27,21 +31,31 @@ public class Floor {
             }
         }
 
-        //loop through each element in the array and count +1 for ( and -1 for )
+        //loop through each element in the array and increase or decrease whichFloor by +1 for ( and -1 for )
         for (char ch : chars) {
             if (ch == '(') {
-                count++;
+                whichFloor++;
             } else if (ch == ')') {
-                count--;
+                whichFloor--;
+            }
+
+            //Determine which position user first enters the basement and store in a list
+            if (whichFloor != -1) {
+                position++;
+            } else {
+                positionsList.add(position);
             }
         }
 
-        //return the value of count which represents which floor you're on
-        return count;
+        //Retrieve the first element from the list, since that's the first time we entered the basement
+        System.out.println("The first position you entered the basement is: " + positionsList.get(0));
+
+        //return the value of whichFloor which represents which floor you're on
+        return whichFloor;
     }
 
     public static void main(String[] args) {
         //Test )())()) and should get -3
-        System.out.println(find_floor(")())())")); //results in -3
+        System.out.println("You're on floor: " + find_floor(")))"));
     }
 }
